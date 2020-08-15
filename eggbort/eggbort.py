46a6bd32dd08581a23b62f5f42f128840256c34a
Rahul Kiefer <rdk750@gmail.com>
2020-08-15 02:24:54 -0400

    Created an Eggbort class to clean up file code

--- eggbort/eggbort.py
+++ eggbort/eggbort.py
@@ -10,22 +10,44 @@ from discord.ext import commands
 # My file imports
 import file_paths
 
+initial_extensions = (
+    'cogs.bot_properties',
+    'cogs.chat_management',
+    'cogs.debugging',
+    'cogs.events',
+    'cogs.help',
+    'cogs.music',
+)
+
+
 def retrieve_prefix(bot, message):
-  """Returns server prefix for current server on bot startup"""
+    """Returns server prefix for current server on bot startup"""
 
-  with open(file_paths.SERVER_PREFIXES) as f:
-    server_prefixes = json.load(f)
+    with open(file_paths.SERVER_PREFIXES) as f:
+        server_prefixes = json.load(f)
 
-  return ('egg!', server_prefixes[str(message.guild.id)])
+    return ('egg!', server_prefixes[str(message.guild.id)])
 
-bot = commands.Bot(command_prefix=retrieve_prefix)
+
+class Eggbort(commands.Bot):
+    def __init__(self):
+        super().__init__(command_prefix=retrieve_prefix)
+
+        for extension in initial_extensions:
+            try:
+                self.load_extension(extension)
+            except Exception as _e:
+                print(f'Failed to load extension {extension}.')
+
+    def run(self):
+        super().run(
+            os.environ('EGGBORT_TOKEN'),
+            reconnect=True,
+        )
+
+
+bot = Eggbort()
+bot.run()
+
 # bot.remove_command('help')  # removes the default help command
 # TODO implement new help command and uncomment line above
-
-# Loads all extensions (Cogs)
-for filename in os.listdir('eggbort/cogs'):
-  if filename.endswith('.py'):
-    bot.load_extension('cogs.{}'.format(filename[:-3]))  # [:-3] gets rid of .py
-
-
-bot.run(os.environ['EGGBORT_TOKEN'])
