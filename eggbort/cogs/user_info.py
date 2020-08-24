62112c1ec117ecf7084d4680b6f12b7e4f1afd23
Rahul Kiefer <rdk750@gmail.com>
2020-08-24 03:35:25 -0400

    The user's avatar should now be able to be copied

--- eggbort/cogs/user_info.py
+++ eggbort/cogs/user_info.py
@@ -1,4 +1,12 @@
+# built-in library imports
+import io
+
+# discord.py imports
+import discord.file
 from discord.ext import commands
+
+# external packages
+import aiohttp
 
 
 class UserInfo(commands.Cog):
@@ -6,12 +14,21 @@ class UserInfo(commands.Cog):
 
     def __init__(self, bot):
         self.bot = bot
-    
+
     @commands.command()
     async def avatar(self, ctx):
         '''Sends the user's avatar.'''
-        url = str(ctx.message.mentions[0].avatar_url) + '?size=256'
-        await ctx.send(url)
+        user = ctx.message.mentions[0]
+        url = f'{user.avatar_url}?size=256'
+
+        async with aiohttp.ClientSession() as session:
+            async with session.get(url) as resp:
+                if resp.status != 200:
+                    return await ctx.send('Could not download file...')
+                data = io.BytesIO(await resp.read())
+                await ctx.send(
+                    file=discord.File(data, f'{user.name}_avatar.jpg')
+                )
 
     @commands.command()
     async def joindate(self, ctx):
