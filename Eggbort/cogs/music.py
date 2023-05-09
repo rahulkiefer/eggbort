79789723c6922da3ac92ebb27393b735ae7f70f3
Rahul Kiefer <rahul.kiefer@gmail.com>
2023-05-09 18:25:42 -0400

    Initial addition of YT playlist functionality

--- Eggbort/cogs/music.py
+++ Eggbort/cogs/music.py
@@ -25,20 +25,57 @@ class Music(commands.Cog):  # TODO add functionality for Spotify and Soundcloud 
     async def play(self, interaction: discord.Interaction, query_or_link: str):
         """Play a song with the given search query.
 
-        If not connected, connect to our voice channel.
+        If not connected, connect to our voice channel. If a YouTube playlist 
+        link is specified, queues all tracks in the playlist.
         """
         if interaction.user.voice is None:
             return await interaction.response.send_message("You are not in a voice channel.", ephemeral=True, delete_after=5)
         vc: CustomPlayer = interaction.guild.voice_client or await interaction.user.voice.channel.connect(cls=self.player, self_deaf=True)
 
-        track = await wavelink.YouTubeTrack.search(query_or_link, return_first=True)
-
-        if vc.queue.is_empty and not vc.is_playing():
-            await vc.play(track)
-            await interaction.response.send_message(f'Playing `{track.title}`.')
+        if query_or_link.startswith('https://www.youtube.com/playlist'):
+            await playYouTubePlaylist(interaction, self.player, query_or_link)
         else:
-            await vc.queue.put_wait(track)
-            await interaction.response.send_message(f'Added `{track.title}` to the queue...', delete_after=10)
+            track = await wavelink.YouTubeTrack.search(query_or_link, return_first=True)
+
+            if vc.queue.is_empty and not vc.is_playing():
+                await vc.play(track)
+                await interaction.response.send_message(f'Playing `{track.title}`.')
+            else:
+                await vc.queue.put_wait(track)
+                await interaction.response.send_message(f'Added `{track.title}` to the queue...', delete_after=10)
+
+    async def playYouTubePlaylist(self, interaction: discord.Interaction, vc: CustomPlayer, link: str):
+        embed = discord.Embed(description="Searching", color=discord.Color.from_str("rgb(0,0,0)"))
+        await interaction.followup.send(embed=embed)
+        try:
+            embed = discord.Embed(description=f"[Playlist added to queue]({query})", color=discord.Color.from_str("rgb(0,0,0)"))
+            playlist = await wavelink.YouTubePlaylist.search(query)
+            tracks: list[wavelink.YouTubeTrack] = playlist.tracks
+            songs = len(tracks)
+            length = sum(track.length for track in tracks)
+            if not vc.is_playing():
+                for track in tracks:
+                    vc.queue.put(track)
+                first_track = vc.queue.get()
+                embed.add_field(name="Now Playing", value=f"{first_track.title} by {first_track.author}")
+                embed.set_image(url=first_track.thumbnail)
+                await vc.play(first_track)
+            else:
+                for track in tracks:
+                    vc.queue.put(track)
+            minutes, seconds = divmod(length / 1000, 60)
+            hours, minutes = divmod(minutes, 60)
+            if hours > 0:
+                duration = f"{int(hours)}:{int(minutes):02d}:{int(seconds):02d}"
+            else:
+                duration = f"{int(minutes):02d}:{int(seconds):02d}"
+            
+            embed.add_field(name="Enqueued", value=f"{songs} songs")
+            embed.set_footer(text=f"Playlist Duration: {duration}")
+            await interaction.edit_original_response(embed=embed)
+        except Exception:
+            embed = discord.Embed(description=f"{query} is an invalid link/playlist!", color=discord.Color.red())
+            await interaction.edit_original_response(embed=embed)
 
     @app_commands.command(name='pause', description="Pause the player") # TODO can I delete the message after playing is resumed?
     @app_commands.checks.has_permissions(speak=True)
